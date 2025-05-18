4c9a98a5fa09abdb97233c399906780c7e8f03b1
Anastasia-Mialitsina <malicynaanastasia@gmail.com>
2025-05-18 19:25:35 +0300

    all

--- src/server.js
+++ src/server.js
@@ -1,10 +1,32 @@
+// const express = require('express');
+// const logger = require('morgan');
+// const cors = require('cors');
+// const contactsRouter = require('./routers/contacts');
+// const errorHandler = require('./middlewares/errorHandler');
+// const notFoundHandler = require('./middlewares/notFoundHandler');
+
+// const app = express();
+
+// app.use(logger('dev'));
+// app.use(cors());
+// app.use(express.json());
+
+// app.use('/api/contacts', contactsRouter);
+
+// app.use(notFoundHandler);
+// app.use(errorHandler);
+
+// module.exports = app;
+
+
 // src/server.js
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-const contactsRouter = require('./routers/contacts');
-const errorHandler = require('./middlewares/errorHandler');
-const notFoundHandler = require('./middlewares/notFoundHandler');
+import express from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+
+import contactsRouter from './routers/contacts.js';
+import errorHandler from './middlewares/errorHandler.js';
+import notFoundHandler from './middlewares/notFoundHandler.js';
 
 const app = express();
 
@@ -17,4 +39,10 @@ app.use('/api/contacts', contactsRouter);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-module.exports = app;
+
+export const setupServer = () => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
