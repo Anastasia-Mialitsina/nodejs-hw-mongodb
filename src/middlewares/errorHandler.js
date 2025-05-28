7130774d5d3f8901945c1e9769bc564cadff4651
Anastasia-Mialitsina <malicynaanastasia@gmail.com>
2025-05-28 12:14:14 +0300

    all

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,14 +1,15 @@
 // src/middlewares/errorHandler.js
-
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   const { status = 500, message = 'Something went wrong' } = err;
+
   res.status(status).json({
     status,
     message,
-    data: err.message,
+    data: null, 
   });
 };
 
 export default errorHandler;
+
 
