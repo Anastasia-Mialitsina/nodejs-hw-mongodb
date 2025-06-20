c3371c7a2a8c93c611730d439536e72fabe00c9e
Anastasia-Mialitsina <malicynaanastasia@gmail.com>
2025-06-20 19:58:14 +0300

    all

--- src/server.js
+++ src/server.js
@@ -50,10 +50,8 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
-
 import contactsRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
-
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 
