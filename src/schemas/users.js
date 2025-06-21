97462270d0e99cf8440aed0876cd3e88da0143bb
Anastasia-Mialitsina <malicynaanastasia@gmail.com>
2025-06-21 21:03:42 +0300

    all

--- src/schemas/users.js
+++ src/schemas/users.js
@@ -1,20 +1,4 @@
 //src/schemas/users.js
-// import Joi from 'joi';
-
-// export const registerSchema = Joi.object({
-//   name: Joi.string().required(),
-//   email: Joi.string().email().required(),
-//   password: Joi.string().min(6).required(),
-// });
-
-// export const loginSchema = Joi.object({
-//   email: Joi.string().email().required(),
-//   password: Joi.string().min(6).required(),
-// });
-
-
-//6
-//src/schemas/users.js
 import Joi from 'joi';
 
 export const registerSchema = Joi.object({
